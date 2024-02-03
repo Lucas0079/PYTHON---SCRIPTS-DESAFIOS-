b9d2af5f7a8fdd2de2830bba9ea757be30e42304
Lucas0079 <sofetebr2018@gmail.com>
2024-02-03 16:40:59 -0300

    primeiro commit

---
+++ Desafio 01.py
@@ -0,0 +1,4 @@
+print('====== DESAFIO 01 ======')
+nome = input('Digite o seu nome: ')
+print('Seja bem-vindo(a)',nome,"!")
+
---
+++ Desafio 02.py
@@ -0,0 +1,5 @@
+print('====== DESAFIO 02 ======')
+dia = input('Informe o dia do seu nascimento: ')
+mes = input('Informe o mês do seu nascimento: ')
+ano = input('Informe o ano do seu nascimento: ')
+print('Você nasceu no dia ',dia,'de ',mes,'de ',ano)
